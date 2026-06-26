8f3c2da77c0d08ceebdb6c99ed1d2570c5c643c2
karlaemartins <karlaemartinss@gmail.com>
2026-06-26 08:54:27 -0300

    feat: implement image filter service

--- Project13-InstaFilter/Core/Services/FilterService.swift
+++ Project13-InstaFilter/Core/Services/FilterService.swift
@@ -17,7 +17,37 @@ final class FilterService: FilterServiceProtocol {
         guard let beginImage = CIImage(image: image) else {
             return nil
         }
+        
+        guard let currentFilter = CIFilter(name: filter.coreImageName) else {
+            return nil
+        }
+        
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        let inputKeys = currentFilter.inputKeys
+        
+        
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(intensity, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(intensity * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(CIVector(x: beginImage.extent.midX, y: beginImage.extent.midY), forKey: kCIInputCenterKey)
+        }
+        
+        guard let outputImage = currentFilter.outputImage else {
+            return nil
+        }
 
-        return nil
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+            return nil
+        }
+
+        return UIImage(cgImage: cgImage)
     }
 }
